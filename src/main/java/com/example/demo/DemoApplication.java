1e8102251d8b03d7d02e21e9b737949657f8b640
Gil Piven <glebpiv@gmail.com>
2021-02-18 16:26:02 +0200

    Updating the project description comment

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,9 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-"""
+/*
 Gil's Spring Boot demo application
-"""
+*/
 
 
 @SpringBootApplication
