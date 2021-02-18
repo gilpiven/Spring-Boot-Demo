f43db7b890cd3ad2f3ed60d5e4683109eded8c4d
Gil Piven <glebpiv@gmail.com>
2021-02-18 16:11:30 +0200

    Adding a project description

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,11 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+"""
+Gil's Spring Boot demo application
+"""
+
+
 @SpringBootApplication
 public class DemoApplication {
 
